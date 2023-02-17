f82d6215b9c4dcfab62e49e6c265478c94ebd3bb
Ariel <bublicius2010@gmail.com>
2023-02-16 23:48:53 -0300

    feat: cambios de colores tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'my-custom-color': '#fffff',
+        'mi-azul': '#1B4DB1',
+        'mi-amarillo': '#F3F243',
+        'mi-rosa': '#FF64BC',
+        'mi-negro': '#000000',
+        'mi-gris-oscuro': '#1A1E2E',
+        'mi-gris': '#6E6A6C',
+        'mi-gris-claro': '#A7A6A7',
+        'mi-gris-muy-claro': '#D9D9D9',
+        'mi-blanco': '#F8F7FA',
+        'mi-rojo': '#EF3F47',
       },
       screens: {
         xs: '375px',
